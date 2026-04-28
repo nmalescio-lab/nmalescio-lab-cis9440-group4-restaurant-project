efc5a82f0cafe00d9e738bea9a7959773002cc4e
Nicole Malescio <nicolemalescio@gmail.com>
2026-04-28 22:05:21 +0000

    finished dim_location

--- models/assignment_work/marts/shared/dim_location.sql
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -1,63 +1,52 @@
 WITH all_locations AS (
+
    -- Get locations from 311 requests
-   SELECT DISTINCT CAST(borough AS STRING) AS borough,
-   CAST(incident_zip AS INT) AS zipcode,
-   CAST(location_type AS STRING) AS location_type,
-   CAST(community_board AS STRING) AS community_board,
-   CAST(council_district AS STRING) AS council_district,
-   CAST(census_tract AS STRING) AS census_tract,
-   CAST(incident_address AS STRING) AS address,
-   CAST(street_name AS STRING) AS street_name,
-   CAST(bbl AS STRING) AS bbl,
-   CAST(longitude AS INT) AS longitude,
-   CAST(latitude AS INT) AS latitude
-   
+   SELECT DISTINCT 
+       CAST(borough AS STRING) AS borough,
+       CAST(incident_zip AS INT64) AS zipcode,
+       CAST(location_type AS STRING) AS location_type,
+       CAST(REGEXP_EXTRACT(community_board, r'^\d+') AS INT64) AS community_board,
+       CAST(council_district AS INT64) AS council_district,
+       CAST(NULL AS STRING) AS census_tract
 
    FROM {{ ref('stg_nyc_311_restaurant') }}
    WHERE borough IS NOT NULL
 
    UNION DISTINCT
 
-   -- Get dates from restaurant applications
-   SELECT DISTINCT CAST(boro AS STRING) AS borough,
-   CAST(zipcode AS INT) AS zipcode,
-   CAST(community_board AS STRING) AS community_board,
-   CAST(council_district AS STRING) AS council_district,
-   CAST(census_tract AS STRING) AS census_tract,
-   CAST(building AS STRING) AS building_number,
-   CAST(street AS STRING) AS street_name,
-   CAST(bbl AS STRING) AS bbl,
-   CAST(longitude AS INT) AS longitude,
-   CAST(latitude AS INT) AS latitude
+   -- Get locations from restaurant inspections
+   SELECT DISTINCT 
+       CAST(boro AS STRING) AS borough,
+       CAST(zipcode AS INT64) AS zipcode,
+       CAST(NULL AS STRING) AS location_type,
+       CAST(community_board AS INT64) AS community_board,
+       CAST(council_district AS INT64) AS council_district,
+       CAST(census_tract AS STRING) AS census_tract
+
    FROM {{ ref('stg_nyc_restaurant_inspection') }}
-   WHERE borough IS NOT NULL
+   WHERE boro IS NOT NULL
 ),
 
 location_dimension AS (
    SELECT
-       {{ dbt_utils.generate_surrogate_key(['full_date']) }} AS date_key,
+       {{ dbt_utils.generate_surrogate_key([
+           'borough',
+           'zipcode',
+           'location_type',
+           'community_board',
+           'council_district',
+           'census_tract'
+       ]) }} AS location_key,
 
-       full_date,
-       EXTRACT(YEAR FROM full_date) AS year,
-       EXTRACT(QUARTER FROM full_date) AS quarter,
-       EXTRACT(MONTH FROM full_date) AS month,
-       FORMAT_DATE('%B', full_date) AS month_name,
-       CASE
-           WHEN EXTRACT(MONTH FROM full_date) IN (12, 1, 2) THEN 'Winter'
-           WHEN EXTRACT(MONTH FROM full_date) IN (3, 4, 5) THEN 'Spring'
-           WHEN EXTRACT(MONTH FROM full_date) IN (6, 7, 8) THEN 'Summer'
-           WHEN EXTRACT(MONTH FROM full_date) IN (9, 10, 11) THEN 'Fall'
-       END AS season,
-       EXTRACT(DAY FROM full_date) AS day_of_month,
-       EXTRACT(DAYOFWEEK FROM full_date) AS day_of_week,
-       FORMAT_DATE('%A', full_date) AS day_name,
-       EXTRACT(DAYOFWEEK FROM full_date) IN (1, 7) AS is_weekend,
-       CASE
-           WHEN EXTRACT(MONTH FROM full_date) >= 7 THEN EXTRACT(YEAR FROM full_date) + 1
-           ELSE EXTRACT(YEAR FROM full_date)
-       END AS fiscal_year
-   FROM all_dates
-   WHERE full_date IS NOT NULL
+       borough,
+       zipcode,
+       location_type,
+       community_board,
+       council_district,
+       census_tract
+
+   FROM all_locations
+   WHERE borough IS NOT NULL
 )
 
-SELECT * FROM date_dimension
+SELECT * FROM location_dimension
