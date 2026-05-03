849df6f78c9fc44e069a6332efc768675a91bdce
Nicole Malescio <nicolemalescio@gmail.com>
2026-05-03 02:46:13 +0000

    added draft of dim_inspection and dim_restaurant

---
+++ models/assignment_work/marts/restaurant_inspections/dim_inspection.sql
@@ -0,0 +1,27 @@
+WITH all_inspections AS (
+    SELECT DISTINCT
+        inspection_date,
+        inspection_type
+    FROM {{ ref('stg_nyc_restaurant_inspections') }}
+    WHERE inspection_date IS NOT NULL
+),
+inspection_dimension AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['inspection_date', 'inspection_type']) }} AS inspection_key,
+        inspection_date,
+        inspection_type,
+        EXTRACT(YEAR FROM inspection_date) AS year,
+        EXTRACT(QUARTER FROM inspection_date) AS quarter,
+        EXTRACT(MONTH FROM inspection_date) AS month,
+        FORMAT_DATE('%B', inspection_date) AS month_name,
+        EXTRACT(DAY FROM inspection_date) AS day_of_month,
+        EXTRACT(DAYOFWEEK FROM inspection_date) AS day_of_week,
+        FORMAT_DATE('%A', inspection_date) AS day_name,
+        EXTRACT(DAYOFWEEK FROM inspection_date) IN (1, 7) AS is_weekend,
+        CASE
+            WHEN EXTRACT(MONTH FROM inspection_date) >= 7 THEN EXTRACT(YEAR FROM inspection_date) + 1
+            ELSE EXTRACT(YEAR FROM inspection_date)
+        END AS fiscal_year
+    FROM all_inspections
+)
+SELECT * FROM inspection_dimension
---
+++ models/assignment_work/marts/restaurant_inspections/dim_restaurant.sql
@@ -0,0 +1,27 @@
+WITH all_restaurants AS (
+    SELECT DISTINCT
+        camis,
+        dba,
+        cuisine_description,
+        phone,
+        building,
+        street,
+        zipcode,
+        boro
+    FROM {{ ref('stg_nyc_restaurant_inspections') }}
+    WHERE camis IS NOT NULL
+),
+restaurant_dimension AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['camis']) }} AS restaurant_key,
+        camis AS restaurant_id,
+        dba AS restaurant_name,
+        cuisine_description AS cuisine_type,
+        phone,
+        building AS building_number,
+        street AS street_name,
+        zipcode AS zip_code,
+        boro AS borough
+    FROM all_restaurants
+)
+SELECT * FROM restaurant_dimension
