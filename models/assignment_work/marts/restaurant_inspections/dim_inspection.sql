30332621e9571175187ee50caef1d7b47cd72f9e
Nicole Malescio <nicolemalescio@gmail.com>
2026-05-03 03:34:17 +0000

    finished dim_restaurant, updated draft of dim_inspection

--- models/assignment_work/marts/restaurant_inspections/dim_inspection.sql
+++ models/assignment_work/marts/restaurant_inspections/dim_inspection.sql
@@ -1,27 +1,15 @@
 WITH all_inspections AS (
     SELECT DISTINCT
-        inspection_date,
+        CAST(inspection_date AS DATE) AS inspection_date,
         inspection_type
     FROM {{ ref('stg_nyc_restaurant_inspection') }}
     WHERE inspection_date IS NOT NULL
-),
-inspection_dimension AS (
-    SELECT
-        {{ dbt_utils.generate_surrogate_key(['inspection_date', 'inspection_type']) }} AS inspection_key,
-        inspection_date,
-        inspection_type,
-        EXTRACT(YEAR FROM inspection_date) AS year,
-        EXTRACT(QUARTER FROM inspection_date) AS quarter,
-        EXTRACT(MONTH FROM inspection_date) AS month,
-        FORMAT_DATE('%B', inspection_date) AS month_name,
-        EXTRACT(DAY FROM inspection_date) AS day_of_month,
-        EXTRACT(DAYOFWEEK FROM inspection_date) AS day_of_week,
-        FORMAT_DATE('%A', inspection_date) AS day_name,
-        EXTRACT(DAYOFWEEK FROM inspection_date) IN (1, 7) AS is_weekend,
-        CASE
-            WHEN EXTRACT(MONTH FROM inspection_date) >= 7 THEN EXTRACT(YEAR FROM inspection_date) + 1
-            ELSE EXTRACT(YEAR FROM inspection_date)
-        END AS fiscal_year
-    FROM all_inspections
 )
-SELECT * FROM inspection_dimension
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['inspection_date', 'inspection_type']) }} AS inspection_key,
+    inspection_date,
+    inspection_type,
+    EXTRACT(YEAR FROM inspection_date) AS year,
+    EXTRACT(MONTH FROM inspection_date) AS month,
+    EXTRACT(DAY FROM inspection_date) AS day_of_month
+FROM all_inspections
--- models/assignment_work/marts/restaurant_inspections/dim_restaurant.sql
+++ models/assignment_work/marts/restaurant_inspections/dim_restaurant.sql
@@ -1,20 +1,6 @@
 WITH all_restaurants AS (
     SELECT DISTINCT
         camis,
-        dba,
-        cuisine_description,
-        phone,
-        building,
-        street,
-        zipcode,
-        boro
-    FROM {{ ref('stg_nyc_restaurant_inspection') }}
-    WHERE camis IS NOT NULL
-),
-restaurant_dimension AS (
-    SELECT
-        {{ dbt_utils.generate_surrogate_key(['camis']) }} AS restaurant_key,
-        camis AS restaurant_id,
         dba AS restaurant_name,
         cuisine_description AS cuisine_type,
         phone,
@@ -22,6 +8,17 @@ restaurant_dimension AS (
         street AS street_name,
         zipcode AS zip_code,
         boro AS borough
-    FROM all_restaurants
+    FROM {{ ref('stg_nyc_restaurant_inspection') }}
+    WHERE camis IS NOT NULL
 )
-SELECT * FROM restaurant_dimension
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['camis']) }} AS restaurant_key,
+    camis AS restaurant_id,
+    restaurant_name,
+    cuisine_type,
+    phone,
+    building_number,
+    street_name,
+    zip_code,
+    borough
+FROM all_restaurants
